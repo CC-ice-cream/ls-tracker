d3884748ad4cec24931481a37e6f94d3e44598e3
CC-ice-cream <45475641+CC-ice-cream@users.noreply.github.com>
2023-03-26 06:01:03 +0800

    兼容vue

--- src/core/vueTracker.ts
+++ src/core/vueTracker.ts
@@ -7,7 +7,6 @@ import { useVueTracker } from "../utils/index";
 
 export default {
   install: (app: appConfig, options: Options) => {
-    console.log(app);
     if (!options.requestUrl) {
       return new Error("缺少参数 requestUrl");
     }
